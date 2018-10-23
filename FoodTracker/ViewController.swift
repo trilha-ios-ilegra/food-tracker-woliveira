411943d8881651ad998a9079af031fa4f80dec86
Walter Oliveira <42150766+WalterOlivos@users.noreply.github.com>
2018-10-23 14:15:02 -0300

    Feature/basic ui (#2)
    * Commit Initial
    
    * Delete .DS_Store
    
    * Commit Initial
    
    * Basic UI

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Walter Oliveira on 17/10/18.
+//  Copyright © 2018 Walter Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
