59febbb7f2803f93f4f7697f615718cb0dabb0c7
Walter Oliveira <walteroliveira@Walters-Mac-mini.local>
2018-10-25 17:49:18 -0300

    Commit Navigation

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -1,17 +1,26 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 11/10/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
 import UIKit
+
 
 class Meal {
     
     var name: String
-    var photo : UIImage?
+    var photo: UIImage?
     var rating: Int
     
-    init? (name: String, photo: UIImage?, rating: Int) {
-
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
         guard !name.isEmpty else {
             return nil
         }
-        
+
         guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -2,28 +2,24 @@
 //  MealTableViewCell.swift
 //  FoodTracker
 //
-//  Created by Walter Oliveira on 24/10/18.
-//  Copyright © 2018 Walter Oliveira. All rights reserved.
+//  Created by Jane Appleseed on 11/15/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
 //
 
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
-    
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
