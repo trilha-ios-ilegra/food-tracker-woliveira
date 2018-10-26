134f97ce70af8202aaf0eeea82f9a0f13f7db519
Walter Oliveira <42150766+WalterOlivos@users.noreply.github.com>
2018-10-26 16:45:19 -0300

    Allow the app to persist data after closing it (#14)
    * Prepare the Meal.swift to persist data
    
    * Configure the MealTableViewController to persist data if you close the app and open it again

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import os.log
 
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
     
     init?(name: String, photo: UIImage?, rating: Int) {
         
@@ -28,6 +32,33 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+        
+    }
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        self.init(name: name, photo: photo, rating: rating)
         
     }
 }
